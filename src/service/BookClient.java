a515d508840553298447224237257d3316defd03
osem <oksana.semenkova@yahoo.com>
2016-11-27 15:14:05 +0200

    soap clien added

---
+++ src/service/BookClient.java
@@ -0,0 +1,29 @@
+package service;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import java.net.URL;
+
+/**
+ * Created by osemenkova on 11/27/2016.
+ */
+public class BookClient {
+
+    public static void main(String[] args) throws Exception {
+
+        URL url = new URL("http://localhost:9999/service/hello");
+
+        //1st argument service URI, refer to wsdl document above
+        //2nd argument is service name, refer to wsdl document above
+        QName qname = new QName("http://service/", "BookServiceImplService");
+
+        Service service = Service.create(url, qname);
+
+
+        BookService bookService = service.getPort(BookService.class);
+
+        System.out.println(bookService.getBook(1));
+        System.out.println(bookService.deleteBook(1));
+
+    }
+}
